e6da32502ead10da8cd0c8e5d6eca4152cc2d676
asahi <asap1017@docomo.ne.jp>
2021-06-03 12:19:47 +0900

    タグ機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,26 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :category
+
+  has_many :tagmaps, dependent: :destroy
+  has_many :tags, through: :tagmaps
+
+  def save_posts(tags)
+    # 既にタグネームあるか
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    # 共通要素取り出し
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+    # Destroy
+    old_tags.each do |old_name|
+      self.tags.delete Tag.find_by(tag_name:old_name)
+    end
+    # Create
+    new_tags.each do |new_name|
+      post_tag = Tag.find_or_create_by(tag_name:new_name)
+      self.tags << post_tag
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_002750) do
+ActiveRecord::Schema.define(version: 2021_06_03_000937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,22 @@ ActiveRecord::Schema.define(version: 2021_06_01_002750) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "tagmaps", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_tagmaps_on_post_id"
+    t.index ["tag_id"], name: "index_tagmaps_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_name"], name: "index_tags_on_tag_name", unique: true
+  end
+
+  add_foreign_key "tagmaps", "posts"
+  add_foreign_key "tagmaps", "tags"
 end
